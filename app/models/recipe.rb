b59418b8b7a1dfa2794f4a81853452ac42076bcb
Danislav <danito1233@gmail.com>
2019-04-15 13:07:20 +0100

    Paperclip image crop/size fixed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,9 @@ class Recipe < ApplicationRecord
 	validates :title, presence: true, length: { minimum: 2, maximum: 80 }
 	validates :description, presence: true, length: { minimum: 2 }
 
-	has_attached_file :recipe_img, styles: { recipe_index: "250x350>", recipe_show: "325x475>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :recipe_img, styles: { recipe_index: "250x350>", recipe_show: "325x475>" },
+								   convert_options: { recipe_index: "-quality 100 -strip" }, 
+								   default_url: "/images/:style/missing.png"
     validates_attachment_content_type :recipe_img, content_type: /\Aimage\/.*\z/
+    # { recipe_index: "250x350>", recipe_show: "325x475>" }
 end
